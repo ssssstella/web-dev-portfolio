008f57d5281d341cae9d212600ee1fa1097f19fc
ssssstella <wt.su77@gmail.com>
2023-01-03 19:05:20 -0800

    changed image paths

--- digital-business-card/src/components/Footer.jsx
+++ digital-business-card/src/components/Footer.jsx
@@ -3,10 +3,10 @@ import React from "react"
 export default function Footer() {
     return (
         <footer>
-            <img className="footer--icon" src="./src/images/twitter_icon.png" alt="twitter"/>
-            <img className="footer--icon" src="./src/images/facebook_icon.png" alt="facebook"/>
-            <img className="footer--icon" src="./src/images/instagram_icon.png" alt="instagram"/>
-            <img className="footer--icon" src="./src/images/github_icon.png" alt="github"/>
+            <img className="footer--icon" src="/images/twitter_icon.png" alt="twitter"/>
+            <img className="footer--icon" src="/images/facebook_icon.png" alt="facebook"/>
+            <img className="footer--icon" src="/images/instagram_icon.png" alt="instagram"/>
+            <img className="footer--icon" src="/images/github_icon.png" alt="github"/>
         </footer>
     )
 }
--- digital-business-card/src/components/Info.jsx
+++ digital-business-card/src/components/Info.jsx
@@ -3,17 +3,17 @@ import React from "react"
 export default function Info() {
     return (
         <div className="info">
-            <img className="info--profile" src="./src/images/profile.jpg" alt="laura proflie"/>
+            <img className="info--profile" src="/images/profile.jpg" alt="laura proflie"/>
             <h4 className="info--name">Laura Smith</h4>
             <p className="info--occupation">Frontend Developer</p>
             <a href="#" className="info--website small-text">laurasmith.website</a>
             <div className="info--btn-ctn">
                 <button className="info--btn email">
-                    <img className="info--contact" src="./src/images/mail.png" alt="email"/>
+                    <img className="info--contact" src="/images/mail.png" alt="email"/>
                     <a href="#">Email</a>
                 </button>
                 <button className="info--btn linkedin">
-                    <img className="info--contact" src="./src/images/linkedin.png" alt="linkedin"/>
+                    <img className="info--contact" src="/images/linkedin.png" alt="linkedin"/>
                     <a href="#">LinkedIn</a>
                 </button>
             </div>
